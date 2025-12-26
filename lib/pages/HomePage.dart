56cc2facd265d4bfbcaee6fdfbd65dea4f0b550f
Varintor <varintorn_si@cmu.ac.th>
2025-12-26 10:13:54 +0700

    ACT7.2

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:firstshop/pages/apple.dart';
 import 'package:firstshop/pages/detailpage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   // const HomePage({Key? key}) : super(key: key);
@@ -17,90 +20,80 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(title: Text('computer knowledge')),
       body: Padding(
         padding: const EdgeInsets.all(20),
-        child: ListView(
-          children: [
-            mybox(
-              "what is a computer?",
-              "Computer is a things to calculate and fo any other works",
-              "assets/2.jpg",
-              () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Detailpage()),
+        child: FutureBuilder<String>(
+          future: DefaultAssetBundle.of(context).loadString('assets/data.json'),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            if (!snapshot.hasData) {
+              return const Center(child: Text('No data found'));
+            }
+
+            final List<dynamic> data = json.decode(snapshot.data!);
+
+            return ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (BuildContext context, int index) {
+                return mybox(
+                  data[index]['title'],
+                  data[index]['subtitle'],
+                  data[index]['imageUrl'],
                 );
               },
-            ),
-            SizedBox(height: 20),
-            mybox(
-              "what is Flutter?",
-              "Flutter is an open-source UI software development toolkit created by Google.",
-              "assets/3.jpg",
-              () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ApplePage()),
-                );
-              },
-            ),
-            SizedBox(height: 20),
-            mybox(
-              "what is a Dart?",
-              "Dart is a programming language designed for building web, server, desktop, and mobile applications.",
-              "assets/4.jpg",
-              () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ApplePage()),
-                );
-              },
-            ),
-            SizedBox(height: 20),
-          ],
+            );
+          },
         ),
       ),
     );
   }
 
-  Widget mybox(
-    String title,
-    String subtitle,
-    String imageUrl,
-    VoidCallback ontab,
-  ) {
+  Widget mybox(String title, String subtitle, String imageUrl) {
     return GestureDetector(
-      onTap: ontab,
       child: Container(
+        margin: EdgeInsets.only(bottom: 20),
         padding: EdgeInsets.all(20),
         height: 150,
         decoration: BoxDecoration(
           color: Colors.grey[300],
           borderRadius: BorderRadius.circular(20),
-        image: DecorationImage(
-          image: NetworkImage(imageUrl),
-          fit: BoxFit.cover,
-          colorFilter: ColorFilter.mode(
-            Colors.black.withOpacity(0.3),
-            BlendMode.darken,
-          ),
-        ),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            title,
-            style: TextStyle(
-              fontSize: 25,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
+          image: DecorationImage(
+            image: NetworkImage(imageUrl),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+              Colors.black.withOpacity(0.3),
+              BlendMode.darken,
             ),
           ),
-          SizedBox(height: 20),
-          Text(subtitle, style: TextStyle(fontSize: 15, color: Colors.white)),
-          TextButton(onPressed: ontab, child: Text('View Details')),
-        ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(subtitle, style: TextStyle(fontSize: 15, color: Colors.white)),
+            TextButton(
+              onPressed: () {
+                print("nextpage>>");
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Detailpage()),
+                );
+              },
+              child: Text('View Details'),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
